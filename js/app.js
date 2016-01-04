acb1ac98fbcbf1402a2f875ef17a01e6e122c1bd
Pete Considine <pete.considine@gmail.com>
2016-01-03 17:50:07 -0800

    Backup. No substantial changes.

--- js/app.js
+++ js/app.js
@@ -1,39 +1,33 @@
-function search(){
-	$("#effects").html('');
-	var key = "z9QVJMAfnJkGN8V48Cs4pzYUS0XTEqksWsMi0jvr"
-	var searchTerm = document.getElementById("fdaSearch").value
-
-	$.ajax({
-    
-	   	url: "https://api.fda.gov/drug/event.json?api_key="+ key + "&search=" + searchTerm+"&count=patient.reaction.reactionmeddrapt.exact",
-	   	dataType: "json",
-	   	success: function(data) {
-	   		for(i=0;i<15;i++){
-	    		var results = (data.results[i].term)
-          if(data.results[i].term === "DRUG INEFFECTIVE"){
-            results[i].term = ""
-          }else{
-            		$("#effects").append(results + " " + "<br>")
-				console.log(data.results[i].term)
-          }
-	    
+$(document).ready(function() {	
+	$(".btn").click(function() {
+		$.ajax({
+			type: "GET",
+			url: "https://api.fda.gov/drug/event.json?api_key=z9QVJMAfnJkGN8V48Cs4pzYUS0XTEqksWsMi0jvr",
+			mimeType: "application/json",
+			dataType: "json",
+			success: function(data){
+				var results = data.results;
+				var html = "";
+				
+				results.forEach(function(element, index, array) {
+					var Key = Object.keys(element);
+					
+					html += Key[11];
+					
+					var patientKey = Object.keys(Key[11]);
+					
+					html += "<br />" + patientKey;
+					
+					var drugKey = Object.keys(patientKey[0]);
+					
+					html += "<br />" + drugKey;
+					
+				});
+				$(".message").html(html);
+			},
+			error: function(error) {
+				console.log("No data received");
 			}
-	   	},
-	   type: 'GET'
-	});
-}
-
-/*
-$.getJSON("/json/cats.json", function(json) {
-	var html = "";
-	json.forEach(function(val) {
-		var keys = Object.keys(val);
-		html += "<div class = 'cat'>";
-		keys.forEach(function(key) {
-			html += "<b>" + key + "</b>: " + val[key] + "<br />";
-			html += "</div><br />";
 		});
 	});
-	$(".message").html(html);
 });
-*/
