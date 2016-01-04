212a46cc886e3a4c8782a3918f28d1ac2e7b233a
Pete Considine <pete.considine@gmail.com>
2016-01-03 20:52:45 -0800

    Signed-off-by: Pete Considine <pete.considine@gmail.com>

--- js/app.js
+++ js/app.js
@@ -1,28 +1,32 @@
 $(document).ready(function() {	
-	$(".btn").click(function() {
+
+	function resetForm() {
+		$(".form-control").val('');
+		$(".message").html("Enter your search terms")
+	}
+	
+	$("[name=reset]").click (function() {
+		resetForm("search-form");
+	});
+
+	$(".btn-primary").click(function() {
+		var patientAge = ("patient.patientonsetage=" + $('[name=patientAge]').val() + "&");
+		
+		console.log(patientAge);
+
 		$.ajax({
 			type: "GET",
-			url: "https://api.fda.gov/drug/event.json?api_key=z9QVJMAfnJkGN8V48Cs4pzYUS0XTEqksWsMi0jvr",
+			url: "https://api.fda.gov/drug/event.json?api_key=z9QVJMAfnJkGN8V48Cs4pzYUS0XTEqksWsMi0jvr&search=" + patientAge + "limit=10",
 			mimeType: "application/json",
 			dataType: "json",
 			success: function(data){
 				var results = data.results;
 				var html = "";
 				
-				results.forEach(function(element, index, array) {
-					var Key = Object.keys(element);
-					
-					html += Key[11];
-					
-					var patientKey = Object.keys(Key[11]);
-					
-					html += "<br />" + patientKey;
-					
-					var drugKey = Object.keys(patientKey[0]);
-					
-					html += "<br />" + drugKey;
-					
+				results.forEach(function (element, index, array) {
+					html += "<li>Patient reaction: " + element.patient.reaction[0].reactionmeddrapt + "<br />Drug indication: " + element.patient.drug[0].drugindication + "</li>";
 				});
+				
 				$(".message").html(html);
 			},
 			error: function(error) {
